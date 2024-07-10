23868204fd1eef6e7889ab77e9f04487e130bb8f
vibhamaurya05 <vibhamaurya5sep@gmail.com>
2024-07-10 20:32:11 +0530

    fix: email & password session

--- src/protectrouter.tsx
+++ src/protectrouter.tsx
@@ -1,35 +1,3 @@
-// import React, { ReactNode, useEffect, useState } from "react";
-// import { Navigate } from "react-router-dom";
-// import { account } from "./appwrite/appwrite";
-
-// interface ProtectedRouteProps {
-//      children: ReactNode;
-// }
-
-// const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-//      const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-
-//      useEffect(() => {
-//           const checkAuth = async () => {
-//                try {
-//                     await account.get();
-//                     setIsAuthenticated(true);
-//                } catch (error) {
-//                     setIsAuthenticated(false);
-//                }
-//           };
-
-//           checkAuth();
-//      }, []);
-
-//      if (isAuthenticated === null) {
-//           return <div>Loading...</div>; 
-//      }
-
-//      return isAuthenticated ? <>{children}</> : <Navigate to="/login" />;
-// };
-
-// export default ProtectedRoute;
 
 
 
--- src/redirectauth.tsx
+++ src/redirectauth.tsx
@@ -1,35 +1,3 @@
-// import React, { ReactNode, useEffect, useState } from "react";
-// import { Navigate } from "react-router-dom";
-// import { account } from "./appwrite/appwrite";
-
-// interface RedirectAuthenticatedProps {
-//      children: ReactNode;
-// }
-
-// const RedirectAuthenticated: React.FC<RedirectAuthenticatedProps> = ({ children }) => {
-//      const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-
-//      useEffect(() => {
-//           const checkAuth = async () => {
-//                try {
-//                     await account.get();
-//                     setIsAuthenticated(true);
-//                } catch (error) {
-//                     setIsAuthenticated(false);
-//                }
-//           };
-
-//           checkAuth();
-//      }, []);
-
-//      if (isAuthenticated === null) {
-//           return <div>Loading...</div>; 
-//      }
-
-//      return isAuthenticated ? <Navigate to="/" /> : <>{children}</>;
-// };
-
-// export default RedirectAuthenticated;
 
 
 import React, { ReactNode, useEffect, useState } from 'react';
